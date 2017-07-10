e2d2f2daf9c9975ae5582f44ce20f38d015bc30a
sruthi2498 <vsruthi98@gmail.com>
2017-07-10 17:18:07 +0530

    Update Zomato_review_extraction.py

--- Zomato_review_extraction.py
+++ Zomato_review_extraction.py
@@ -13,39 +13,7 @@ zomatoKey = "46678c847517af16b7f56dafd773bcdb"
 fpath=os.path.join("data", "output.csv")
 f=open(fpath,'a+')
 
-#*************************************************************************************************************************************8
 
-'''#GET A LIST OF CATEGORIES 
-CategoriesUrl= "https://developers.zomato.com/api/v2.1/categories?"
-header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": zomatoKey}
-
-categories= requests.get(CategoriesUrl, headers=header)
-print r.status_code
-print r.headers
-print r.content
-r.elapsed       # returns datetime.timedelta(0, 1, 666890)
-r.url           # returns 'https://tutsplus.com/'
- 
-r.history      # returns [<Response [301]>, <Response [301]>]
- 
-r.headers['Content-Type']
-#print categories.json()
-CatJson=categories.json()
-#print CatJson.items()
-#print type(CatJson)
-ListOfCategories=[]
-#print CatJson.values()
-for V in CatJson.values():
-    for i in V:
-        for V2 in i.values():
-            #print type(V2)
-            #print V2
-            for key,val in V2.items():
-                if(key=='name'):
-                    #print val
-                    ListOfCategories.append(val)
-#print ListOfCategories'''
-#*************************************************************************************************************************************8
 
 #GET A LIST OF RES_IDS
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": zomatoKey}
